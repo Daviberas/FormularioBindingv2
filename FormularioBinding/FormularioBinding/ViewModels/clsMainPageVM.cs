c137e7feccba453f928f7ff03fd97fd5ebede288
David <daviberas6@gmail.com>
2016-11-13 16:35:46 +0100

    Falla algo del bindeo pero no veo lo que es

--- FormularioBinding/FormularioBinding/ViewModels/clsMainPageVM.cs
+++ FormularioBinding/FormularioBinding/ViewModels/clsMainPageVM.cs
@@ -1,13 +1,41 @@
-﻿using System;
+﻿using FormularioBinding.Models;
+using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace FormularioBinding.ViewModels
 {
-    class clsMainPageVM
+    public class clsMainPageVM
     {
+        private clsPersona _personaSeleccionada;
+        private ObservableCollection<clsPersona> _listado;
 
+        public clsPersona personaSeleccionada
+        {
+            get
+            {
+                return _personaSeleccionada;
+            }
+            set
+            {
+                _personaSeleccionada = value;
+            }
+        }
+        public ObservableCollection<clsPersona> listado
+        {
+            get
+            {
+                return _listado;
+            }
+        }
+
+        public clsMainPageVM()
+        {
+            clsListado lista = new clsListado();
+            _listado = lista.getListado();
+        }
     }
 }
